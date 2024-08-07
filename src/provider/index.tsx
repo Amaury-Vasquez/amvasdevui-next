0e5d6e155b537abb162d5b3caa3d5952d6c77d3b
Amaury Vasquez <amaury.vasquez@prescriberpoint.com>
2024-08-07 17:11:50 -0600

    feat: add react query

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { CookiesProvider } from "react-cookie";
 import { ReactNode } from "react";
 
@@ -6,10 +8,16 @@ interface ProviderProps {
   children: ReactNode;
 }
 
-const Provider = ({ children }: ProviderProps) => (
-  <CookiesProvider defaultSetOptions={{ path: "/" }}>
-    {children}
-  </CookiesProvider>
-);
+const Provider = ({ children }: ProviderProps) => {
+  const client = new QueryClient();
+  return (
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <QueryClientProvider client={client}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </CookiesProvider>
+  );
+};
 
 export default Provider;
